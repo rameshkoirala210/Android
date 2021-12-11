cd201a04629a67d34a32840ec980f272ad97ce7f
Roshan Mishra <rmishra3@uncc.edu>
2021-12-11 15:11:51 -0500

    cleanup

--- PasswordManager/app/src/main/java/com/example/passwordmanager/ContactFragment.java
+++ PasswordManager/app/src/main/java/com/example/passwordmanager/ContactFragment.java
@@ -8,19 +8,32 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+import javax.annotation.Nullable;
 
 
 public class ContactFragment extends Fragment {
     private FirebaseAuth mAuth;
-    private final String TAG = "Forumstag";
+    private final String TAG = "TAG_ContactFragment";
+    ArrayList<Contact> contactsList = new ArrayList<>();
 
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    FirebaseFirestore db;
     ContactAdapter adapter;
     LinearLayoutManager mLayoutManager;
     RecyclerView recyclerView;
@@ -34,8 +47,8 @@ public class ContactFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_contact, container, false);
         getActivity().setTitle("Contact");
-
         mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
 
         view.findViewById(R.id.buttonLogout).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,12 +61,34 @@ public class ContactFragment extends Fragment {
         view.findViewById(R.id.buttonNewForum).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mListener.gotoNewForumFragment();
+                mListener.gotoNewContactFragment();
             }
         });
 
+        recyclerView = view.findViewById(R.id.container);
+        adapter = new ContactAdapter();
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setHasFixedSize(true);
+        getContacts();
 
         return view;
+    }
+
+    private void getContacts(){
+        db.collection("Contact").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                if (error == null){
+                    contactsList.clear();
+                    for(QueryDocumentSnapshot queryDocumentSnapshot: value) {
+                        Contact contact = queryDocumentSnapshot.toObject(Contact.class);
+                        contactsList.add(contact);
+                    }
+                    recyclerView.setAdapter(adapter);
+                }
+            }
+        });
     }
 
 
@@ -66,7 +101,7 @@ public class ContactFragment extends Fragment {
     }
 
     interface ContactListener{
-        void gotoNewForumFragment();
+        void gotoNewContactFragment();
         void gotoLoginFragmentafterLogout();
     }
 
@@ -74,23 +109,48 @@ public class ContactFragment extends Fragment {
         @NonNull
         @Override
         public ContactHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            return null;
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.contact_list, parent, false);
+            return new ContactHolder(view);
         }
 
         @Override
         public void onBindViewHolder(@NonNull ContactHolder holder, int position) {
-
+            holder.setUpContactItem(contactsList.get(position));
         }
 
         @Override
         public int getItemCount() {
-            return 0;
+            return contactsList.size();
         }
 
         public class ContactHolder extends RecyclerView.ViewHolder {
+            TextView textViewEmail, textViewPassword;
+            ImageView imageViewTrash;
 
             public ContactHolder(@NonNull View itemView) {
                 super(itemView);
+                textViewEmail = itemView.findViewById(R.id.textViewEmail);
+                textViewPassword = itemView.findViewById(R.id.textViewPassword);
+                imageViewTrash = itemView.findViewById(R.id.imageViewtrash);
+            }
+
+            public void setUpContactItem(Contact contact) {
+                textViewEmail.setText(contact.getEmail());
+                textViewPassword.setText(contact.getPassword());
+                imageViewTrash.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //TODO remove contact from firebase
+                        //Log.d(TAG, "onClick: TRYING TO REMOVE" + contact.toString());
+                        db.collection("Contact").document(contact.getDocumentID()).delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d(TAG, "onSuccess: " + contact + " successfully deleted");
+                            }
+                        });
+                    }
+                });
             }
         }
     }
--- PasswordManager/app/src/main/java/com/example/passwordmanager/MainActivity.java
+++ PasswordManager/app/src/main/java/com/example/passwordmanager/MainActivity.java
@@ -3,15 +3,24 @@ package com.example.passwordmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity implements LoginFragment.LoginListener, CreateAccountFragment.NewAccountListener{
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity implements LoginFragment.LoginListener, CreateAccountFragment.NewAccountListener, ContactFragment.ContactListener, NewContactFragment.NewContactFragmentListener {
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        getSupportFragmentManager().beginTransaction().add(R.id.layout, new LoginFragment()).commit();
+        mAuth = FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        if(mAuth.getCurrentUser() == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.layout, new LoginFragment()).commit();
+        } else {
+            getSupportFragmentManager().beginTransaction().add(R.id.layout, new ContactFragment()).commit();
+        }
     }
 
 
@@ -33,5 +42,25 @@ public class MainActivity extends AppCompatActivity implements LoginFragment.Log
     @Override
     public void gotoNewAccountFragment() {
         getSupportFragmentManager().beginTransaction().replace(R.id.layout, new CreateAccountFragment()).addToBackStack(null).commit();
+    }
+
+    @Override
+    public void gotoNewContactFragment() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.layout, new NewContactFragment()).addToBackStack(null).commit();
+    }
+
+    @Override
+    public void gotoLoginFragmentafterLogout() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.layout, new LoginFragment()).commit();
+    }
+
+    @Override
+    public void gotoContactFragmentAfterCancel() {
+        getSupportFragmentManager().popBackStack();
+    }
+
+    @Override
+    public void doneCreateContact() {
+        getSupportFragmentManager().popBackStack();
     }
 }
